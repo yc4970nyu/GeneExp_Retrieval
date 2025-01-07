539e5c400a3c5a8225092b3d0461742b04b1971a
yc4970nyu <144164891+yc4970nyu@users.noreply.github.com>
2025-01-06 19:34:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 # Set page layout
 st.set_page_config(layout="wide", page_title="Gene Expression and Treatment Analysis")
